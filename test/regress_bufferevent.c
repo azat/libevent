a73fb81c6cf010150c540ffd949450f5004ec5a8
Azat Khuzhin <azat@libevent.org>
2024-05-05 21:22:10 +0200

    test: fix bufferevent/bufferevent_connect_fail_eventcb* on FreeBSD
    On FreeBSD the eventcb could be triggered multiple times, one for
    BEV_EVENT_ERROR (usual), and extra for BEV_EVENT_EOF|BEV_EVENT_READING.
    
    This is the truss diff (lines starts with + is success run):
    
                 OK /root/libevent/test/regress_bufferevent.c:886: write(1,"\n  \t OK /root/libevent/test/re"...,55) = 55 (0x37)
         assert(n_reads_invoked == 0): 0 vs 0write(1,"assert(n_reads_invoked == 0): 0 "...,36) = 36 (0x24)
        -kevent(3,{ 5,EVFILT_READ,EV_ADD,0,0,0x30303 5,EVFILT_WRITE,EV_ADD,0,0,0x30303 },2,{ 5,EVFILT_READ,EV_EOF,0,0,0x30303 5,EVFILT_WRITE,EV_EOF,0,0x8000,0x30303 },64,{ 0.000000000 }) = 2 (0x2)
        +kevent(3,{ 5,EVFILT_READ,EV_ADD,0,0,0x30303 5,EVFILT_WRITE,EV_ADD,0,0,0x30303 },2,{ 5,EVFILT_READ,EV_EOF,NOTE_LOWAT|0x3c,0,0x30303 5,EVFILT_WRITE,EV_EOF,NOTE_LOWAT|0x3c,0x8000,0x30303 },64,{ 0.300000000 }) = 2 (0x2)
        +ioctl(5,FIONREAD,0x82106fcc4)                   = 0 (0x0)
        +read(5,0x24e8dd8372b0,16384)                    ERR#61 'Connection refused'
        +getsockopt(5,SOL_SOCKET,SO_ERROR,0x82106fd64,0x82106fd60) = 0 (0x0)
    
        -    OK /root/libevent/test/regress_bufferevent.c:761: write(1,"\n    OK /root/libevent/test/reg"...,55) = 55 (0x37)
        -Read invoked on 5.write(1,"Read invoked on 5.",18)              = 18 (0x12)
        -ioctl(5,FIONREAD,0x820c21264)                   = 0 (0x0)
        -read(5,0x178fdee37330,16384)                    = 0 (0x0)
        -
             OK /root/libevent/test/regress_bufferevent.c:754: write(1,"\n    OK /root/libevent/test/reg"...,55) = 55 (0x37)
        -Read eventcb simple invoked on 5 (what=17).write(1,"Read eventcb simple invoked on 5"...,43) = 43 (0x2b)
        -getsockopt(5,SOL_SOCKET,SO_ERROR,0x820c21304,0x820c21300) = 0 (0x0)
        -
        -    OK /root/libevent/test/regress_bufferevent.c:754: write(1,"\n    OK /root/libevent/test/reg"...,55) = 55 (0x37)
         Read eventcb simple invoked on 5 (what=32).write(1,"Read eventcb simple invoked on 5"...,43) = 43 (0x2b)
         kevent(3,{ 5,EVFILT_READ,EV_DELETE,0,0,0x0 5,EVFILT_WRITE,EV_DELETE,0,0,0x0 },2,{ },64,{ 0.300000000 }) = 0 (0x0)
         close(4)                                        = 0 (0x0)
    
        -  FAIL /root/libevent/test/regress_bufferevent.c:895: write(1,"\n  FAIL /root/libevent/test/reg"...,55) = 55 (0x37)
        -assert(n_events_invoked == 1): 2 vs 1write(1,"assert(n_events_invoked == 1): 2"...,37) = 37 (0x25)
        -setitimer(ITIMER_REAL,{ 0.000000, 0.000000 },{ 0.000000, 29.683878 }) = 0 (0x0)
        +        OK /root/libevent/test/regress_bufferevent.c:895: write(1,"\n  \t OK /root/libevent/test/re"...,55) = 55 (0x37)
        +assert(n_events_invoked == 1): 1 vs 1write(1,"assert(n_events_invoked == 1): 1"...,37) = 37 (0x25)
        +
        +        OK /root/libevent/test/regress_bufferevent.c:896: write(1,"\n  \t OK /root/libevent/test/re"...,55) = 55 (0x37)
        +assert(n_reads_invoked == 0): 0 vs 0write(1,"assert(n_reads_invoked == 0): 0 "...,36) = 36 (0x24)
        +setitimer(ITIMER_REAL,{ 0.000000, 0.000000 },{ 0.000000, 29.677520 }) = 0 (0x0)
         close(5)                                        = 0 (0x0)
        -getpid()                                        = 29718 (0x7416)
        +getpid()                                        = 31514 (0x7b1a)
         close(3)                                        = 0 (0x0)
    
        -  [FAILED bufferevent/bufferevent_connect_fail_eventcb (3 retries)]
        -write(1,"\n  [FAILED bufferevent/bufferev"...,69) = 69 (0x45)
        -1/1 TESTS FAILED. (0 skipped)
        -write(1,"1/1 TESTS FAILED. (0 skipped)\n",30)   = 30 (0x1e)
        -exit(0x1)
        -process exit, rval = 1
        +write(1,"\n",1)                                         = 1 (0x1)
        +1 tests ok.  (0 skipped)
        +write(1,"1 tests ok.  (0 skipped)\n",25)        = 25 (0x19)
        +exit(0x0)
        +process exit, rval = 0

--- test/regress_bufferevent.c
+++ test/regress_bufferevent.c
@@ -896,8 +896,14 @@ test_bufferevent_connect_fail_eventcb(void *arg)
 	event_add(&close_listener_event, &close_timeout);
 
 	event_base_dispatch(data->base);
-	tt_int_op(n_events_invoked, ==, 1);
-	tt_int_op(n_reads_invoked, ==, 0);
+	/* NOTE: on FreeBSD it could be triggered multiple times:
+	 * - BEV_EVENT_ERROR
+	 * - BEV_EVENT_EOF|BEV_EVENT_READING (this is extra for FreeBSD, and one
+	 *   read event also will be triggered for this case)
+	 * FIXME: shouldn't we ignore it on a bufferevent layer?
+	 */
+	tt_assert(n_events_invoked == 1 || n_events_invoked == 2);
+	tt_assert(n_reads_invoked == 0 || n_reads_invoked == 1);
 
 end:
 	if (lev)
