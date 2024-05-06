azat/libevent

commit 9a0a3a3e6510b67c378aade2e8e6097b70ff6daa

be: fix with filtered bufferevents and connect() without EAGAIN
With filtered bufferevents (i.e. not real one, that have socket), we can
trigger incorrect callback in this case. Let's look at example with http
and bufferevent_openssl_filter_new():
- bev = bufferevent_openssl_filter_new()
- http layer trying to connect() to localhost with bev
  # at this time, bev have writecb/readcb NULL but ev_write/ev_read has
  # timeout with 45 secs, default HTTP connect timeout
- and when connect() retruns without EAGAIN (BSD'ism) we called
  event_active() before (with EV_WRITE), and this will call ev_write
  timeout only, while it is more correct to act on bufferevent instead
  of plain event, so let's trigger EV_WRITE for bufferevent which will
  do the job (and let's do this deferred).

Fixes: http/https_simple # under solaris

bufferevent_sock.c
@@ -249,8 +249,8 @@ bufferevent_writecb(evutil_socket_t fd, short event, void *arg)
 		/* we need to fake the error if the connection was refused
 		 * immediately - usually connection to localhost on BSD */
 		if (bufev_p->connection_refused) {
-		  bufev_p->connection_refused = 0;
-		  c = -1;
+			bufev_p->connection_refused = 0;
+			c = -1;
 		}
 
 		if (c == 0)
@@ -438,13 +438,12 @@ bufferevent_socket_connect(struct bufferevent *bev,
 		/* The connect succeeded already. How very BSD of it. */
 		result = 0;
 		bufev_p->connecting = 1;
-		event_active(&bev->ev_write, EV_WRITE, 1);
+		bufferevent_trigger_nolock_(bev, EV_WRITE, BEV_OPT_DEFER_CALLBACKS);
 	} else {
 		/* The connect failed already.  How very BSD of it. */
-		bufev_p->connection_refused = 1;
-		bufev_p->connecting = 1;
 		result = 0;
-		event_active(&bev->ev_write, EV_WRITE, 1);
+		bufferevent_run_eventcb_(bev, BEV_EVENT_ERROR, BEV_OPT_DEFER_CALLBACKS);
+		bufferevent_disable(bev, EV_WRITE|EV_READ);
 	}
 
 	goto done;

commit 9c79c7d8735e3ae32fbdd882c598d1701df5d5f6

be: fix bufferevent_socket_connect in case of inplace connect() (BSD)
In case of inplace connect() we should trigger the error and not rely on
that bufferevent_writecb will detect with SO_ERROR, since it is not
always determine:

    $ truss bin/regress --verbose --no-fork bufferevent/bufferevent_connect_fail
    socket(PF_INET,SOCK_STREAM,0)                    = 4 (0x4)

        OK /root/libevent/test/regress_bufferevent.c:711: write(1,"\n    OK /root/libevent/test/reg"...,55) = 55 (0x37)
    assert(fd >= 0)write(1,"assert(fd >= 0)",15)                     = 15 (0xf)
    bind(4,{ AF_INET 127.0.0.1:0 },16)               = 0 (0x0)

        OK /root/libevent/test/regress_bufferevent.c:712: write(1,"\n    OK /root/libevent/test/reg"...,55) = 55 (0x37)
    assert(bind(fd, sa, slen) == 0)write(1,"assert(bind(fd, sa, slen) == 0)",31)     = 31 (0x1f)
    getsockname(4,{ AF_INET 127.0.0.1:55398 },0x820fbe260) = 0 (0x0)

        OK /root/libevent/test/regress_bufferevent.c:713: write(1,"\n    OK /root/libevent/test/reg"...,55) = 55 (0x37)
    assert(getsockname(fd, sa, &slen) == 0)write(1,"assert(getsockname(fd, sa, &slen"...,39) = 39 (0x27)

        OK /root/libevent/test/regress_bufferevent.c:945: write(1,"\n    OK /root/libevent/test/reg"...,55) = 55 (0x37)
    assert(bev)write(1,"assert(bev)",11)                     = 11 (0xb)

             OK /root/libevent/test/regress_bufferevent.c:948: write(1,"\n  \t OK /root/libevent/test/re"...,55) = 55 (0x37)
    assert(test_ok == 0): 0 vs 0write(1,"assert(test_ok == 0): 0 vs 0",28)   = 28 (0x1c)
    socket(PF_INET,SOCK_STREAM|SOCK_NONBLOCK,0)      = 5 (0x5)
    connect(5,{ AF_INET 127.0.0.1:55398 },16)        ERR#61 'Connection refused'

        OK /root/libevent/test/regress_bufferevent.c:952: write(1,"\n    OK /root/libevent/test/reg"...,55) = 55 (0x37)
    want(r == 0 || r == -1)write(1,"want(r == 0 || r == -1)",23)             = 23 (0x17)
    kevent(3,{ 5,EVFILT_WRITE,EV_ADD,0,0,0x30303 },1,{ 5,EVFILT_WRITE,EV_EOF,0,0x8000,0x30303 },64,{ 0.300000000 }) = 1 (0x1)
    getsockopt(5,SOL_SOCKET,SO_ERROR,0x820fbe014,0x820fbe010) = 0 (0x0)

      FAIL /root/libevent/test/regress_bufferevent.c:921: write(1,"\n  FAIL /root/libevent/test/reg"...,55) = 55 (0x37)
    didn't fail? what 128write(1,"didn't fail? what 128",21)                 = 21 (0x15)

      FAIL /root/libevent/test/regress_bufferevent.c:963: write(1,"\n  FAIL /root/libevent/test/reg"...,55) = 55 (0x37)
    assert(test_ok == 1): 0 vs 1write(1,"assert(test_ok == 1): 0 vs 1",28)   = 28 (0x1c)
    close(4)                                         = 0 (0x0)
    setitimer(ITIMER_REAL,{ 0.000000, 0.000000 },{ 0.000000, 29.998541 }) = 0 (0x0)
    close(5)                                         = 0 (0x0)
    getpid()                                         = 26840 (0x68d8)
    close(3)                                         = 0 (0x0)

      [FAILED bufferevent/bufferevent_connect_fail (3 retries)]
    write(1,"\n  [FAILED bufferevent/bufferev"...,61) = 61 (0x3d)
    1/1 TESTS FAILED. (0 skipped)
    write(1,"1/1 TESTS FAILED. (0 skipped)\n",30)    = 30 (0x1e)
    exit(0x1)
    process exit, rval = 1

Note, that here even though connect() return -61, the
getsockopt(SO_ERROR) still returns 0.

Fixes: 9a0a3a3e6510b67c378aade2e8e6097b70ff6daa ("be: fix with filtered bufferevents and connect() without EAGAIN")

bufferevent_sock.c
@@ -385,8 +385,8 @@ bufferevent_socket_connect(struct bufferevent *bev,
 	struct bufferevent_private *bufev_p = BEV_UPCAST(bev);
 
 	evutil_socket_t fd;
-	int r = 0;
-	int result=-1;
+	int connect_status = 0;
+	int result = -1;
 	int ownfd = 0;
 
 	bufferevent_incref_and_lock_(bev);
@@ -405,46 +405,57 @@ bufferevent_socket_connect(struct bufferevent *bev,
 #ifdef _WIN32
 		if (bufferevent_async_can_connect_(bev)) {
 			bufferevent_setfd(bev, fd);
-			r = bufferevent_async_connect_(bev, fd, sa, socklen);
-			if (r < 0)
-				goto freesock;
-			bufev_p->connecting = 1;
-			result = 0;
-			goto done;
-		} else
+			connect_status = bufferevent_async_connect_(bev, fd, sa, socklen);
+		} else {
 #endif
-		r = evutil_socket_connect_(&fd, sa, socklen);
-		if (r < 0)
-			goto freesock;
+			connect_status = evutil_socket_connect_(&fd, sa, socklen);
+#ifdef _WIN32
+		}
+#endif
 	}
 #ifdef _WIN32
-	/* ConnectEx() isn't always around, even when IOCP is enabled.
-	 * Here, we borrow the socket object's write handler to fall back
-	 * on a non-blocking connect() when ConnectEx() is unavailable. */
-	if (BEV_IS_ASYNC(bev)) {
-		event_assign(&bev->ev_write, bev->ev_base, fd,
-		    EV_WRITE|EV_PERSIST|EV_FINALIZE, bufferevent_writecb, bev);
+	if (connect_status >= 0)
+	{
+		/* ConnectEx() isn't always around, even when IOCP is enabled.
+		 * Here, we borrow the socket object's write handler to fall back
+		 * on a non-blocking connect() when ConnectEx() is unavailable. */
+		if (BEV_IS_ASYNC(bev)) {
+			event_assign(&bev->ev_write, bev->ev_base, fd,
+				EV_WRITE|EV_PERSIST|EV_FINALIZE, bufferevent_writecb, bev);
+		}
 	}
 #endif
 	bufferevent_setfd(bev, fd);
-	if (r == 0) {
-		if (! be_socket_enable(bev, EV_WRITE)) {
-			bufev_p->connecting = 1;
-			result = 0;
-			goto done;
-		}
-	} else {
+	switch (connect_status) {
+		/* Connection in progress */
+		case 0:
+			if (!be_socket_enable(bev, EV_WRITE)) {
+				bufev_p->connecting = 1;
+				result = 0;
+			}
+			break;
 		/* The connect succeeded already. How very BSD of it. */
-		result = 0;
-		bufev_p->connecting = 1;
-		bufferevent_trigger_nolock_(bev, EV_WRITE, BEV_OPT_DEFER_CALLBACKS);
+		case 1:
+			result = 0;
+			bufev_p->connecting = 1;
+			bufferevent_trigger_nolock_(bev, EV_WRITE, BEV_OPT_DEFER_CALLBACKS);
+			break;
+		/* connect already returns an error (BSDism) */
+		case -1:
+		case 2: /* ECONNREFUSED */
+			if (ownfd)
+				evutil_closesocket(fd);
+
+			result = -1;
+			/* connecting=1 and connection_refused=1 to call the errocb from writecb */
+			bufev_p->connecting = 1;
+			bufev_p->connection_refused = 1;
+			/* NOTE: historically we calls writecb in case of ECONNREFUSED */
+			if (connect_status == 2)
+				bufferevent_trigger_nolock_(bev, EV_WRITE, BEV_OPT_DEFER_CALLBACKS);
+			break;
 	}
 
-	goto done;
-
-freesock:
-	if (ownfd)
-		evutil_closesocket(fd);
 done:
 	bufferevent_decref_and_unlock_(bev);
 	return result;